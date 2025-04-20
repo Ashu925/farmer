79b06cbdde9b206ad05292d16497c29ab5d8817a
Frotaam <sidsharma1760@gmail.com>
2025-04-21 01:39:53 +0530

    .|.

---
+++ services/cokkiechecker.js
@@ -0,0 +1,24 @@
+const { validator } = require("./jwtcreater");
+
+const checkuser=async (req,res,next) => {
+    try {
+        const token=req.cookies.token
+        if(!token){
+            return res.render('home')
+        }
+        const payload=validator(token)
+        if (!payload){
+            return res.render('home')
+        }
+        req.user=payload
+        console.log("user foundd")
+        return next()
+    } catch (error) {
+        console.log("error",error)
+        return res.send('listings')
+    }
+}
+
+module.exports={
+    checkuser
+}
